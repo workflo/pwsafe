0740df2222fccfbc3615fd4aa30cfc3d5b0432b1
Florian Wolff <florian@donuz.de>
2017-01-22 17:45:50 +0100

    New service module: KeyGenerationService

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-require 'openssl'
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -9,17 +7,9 @@ class User < ApplicationRecord
 
   before_create :generate_and_encrypt_key_pair
 
-
   private
 
   def generate_and_encrypt_key_pair
-  	new_rsa_key = OpenSSL::PKey::RSA.new(2048)
-  	cipher = OpenSSL::Cipher::Cipher.new('des3')
-
-  	self.encrypted_private_key = new_rsa_key.to_pem(cipher, self.password)
-	self.public_key = new_rsa_key.public_key.to_pem
-	self.first_name = "Test #{id}"
-
-  	# Rails.logger.debug("generate_and_encrypt_key_pair: #{self.password}\n\n#{self.encrypted_private_key}\n\n#{self.public_key}")
+    KeyGenerationService::generate_and_encrypt_user_key_pair self, password
   end
 end
---
+++ lib/pwsafe/key_generation_service.rb
@@ -0,0 +1,13 @@
+module KeyGenerationService
+  require 'openssl'
+
+
+  def self.generate_and_encrypt_user_key_pair(user, password)
+    new_rsa_key = OpenSSL::PKey::RSA.new(2048)
+    cipher = OpenSSL::Cipher::Cipher.new('des3')
+
+    user.encrypted_private_key = new_rsa_key.to_pem(cipher, password)
+    user.public_key = new_rsa_key.public_key.to_pem
+  end
+
+end
