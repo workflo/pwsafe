b704335e9701de384531ffd64d201a6817b36bf1
Florian Wolff <florian@donuz.de>
2017-01-25 20:24:40 +0100

    Documents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122160700) do
+ActiveRecord::Schema.define(version: 20170122183023) do
+
+  create_table "documents", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
